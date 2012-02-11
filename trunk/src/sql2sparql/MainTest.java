ac7b8b7ec633e13d8dc4803c4ca8ee563ef059a7
anechiteidanielalexandru <anechiteidanielalexandru@a83d394d-860f-64a3-328a-2da6199dd0b9>
2012-02-11 14:26:48 +0000

    Added where..

--- trunk/src/sql2sparql/MainTest.java
+++ trunk/src/sql2sparql/MainTest.java
@@ -6,9 +6,16 @@ public class MainTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+	//	System.out
+	//			.println(SELECTStatementProcessor
+	//					.processQuery("Select nume, prenume from tabel GROUP BY nume ORDER by prenume DESC, nume LIMIT 30"));
+		
 		System.out
 				.println(SELECTStatementProcessor
 						.processQuery("Select nume, prenume from tabel GROUP BY nume ORDER by prenume DESC, nume LIMIT 30"));
+	System.out
+		.println(SELECTStatementProcessor
+				.processQuery("Select nume, prenume from tabel where nume=Daniel or nume LIKE Daniel and prenume=\"Alexandru\" "));
 	}
-
+//and prenume=masa or prenume=\"alexandru\"
 }
